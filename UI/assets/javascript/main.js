22315693743f07b313bdff2efa98e9e7701638f4
Sostene Munezero Bagira <bagirasostene@gmail.com>
2022-03-21 18:03:24 +0200

    Ft: added admin pages

--- UI/assets/javascript/main.js
+++ UI/assets/javascript/main.js
@@ -1,17 +1,5 @@
 const toggler = document.querySelector("#toggler");
 const nav = document.querySelector("#navigation");
-const workSvg = document.querySelectorAll("object.work-svg");
-// const element = new SVGElement();
-// element.getElementsByTagNameNS();
-workSvg.forEach((object) => {
-  console.log(object);
-  object.onload = () => {
-    const realSvg = object.getSVGDocument();
-    const style = realSvg.getElementsByTagName("style")[0];
-    // console.log((style.textContent = ));
-    console.log(realSvg);
-  };
-});
 toggler.addEventListener("click", (e) => {
   e.preventDefault();
   nav.classList.toggle("height-0");
