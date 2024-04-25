a43acba7fbd91436a3c0823d4e6e0c029e7b58d3
Sostene Munezero Bagira <munezerobagira@gmail.com>
2024-04-25 17:01:23 +0200

    change skills to stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const AnimatedStack = ({ items }: { items: string[] }) => {
   const itemsLength = items.length;
   const [activeItemIndex, setActiveItemIndex] = useState(0);
   const data = useMemo(() => items[activeItemIndex], [activeItemIndex, items]);
-  const { } = data;
+  const {} = data;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -32,7 +32,10 @@ const AnimatedStack = ({ items }: { items: string[] }) => {
 
   return (
     <div className="stack">
-      <h1 className="text-4xl sm:text-6xl md:text-8xl  leading-[3rem] md:leading-[7rem] font-bold" id="keywords">
+      <h1
+        className="text-4xl sm:text-6xl md:text-8xl  leading-[3rem] md:leading-[7rem] font-bold"
+        id="keywords"
+      >
         {data}
       </h1>
     </div>
@@ -46,19 +49,19 @@ function Index() {
   const [firstRowSkills, setFirstRowSkills] = useState([]);
   const [secondRowSkills, setSecondRowSkills] = useState([]);
 
-
   useEffect(() => {
     (async () => {
       setOwnerInfo(siteData.user);
       setFirstRowSkills(siteData.skills.filter((_, index) => index % 2 === 0));
       setSecondRowSkills(siteData.skills.filter((_, index) => index % 2 !== 0));
-      setIsLoading(false)
-
+      setIsLoading(false);
     })();
   }, []);
 
   useEffect(() => {
-    const { terminal } = injectTerminal({ container: terminalContainer.current });
+    const { terminal } = injectTerminal({
+      container: terminalContainer.current,
+    });
     terminal;
   }, [terminalContainer]);
   if (isLoading) return <LoadingBall />;
@@ -80,14 +83,25 @@ function Index() {
         <div>
           <div className="text-center py-2">
             <div className="heading">
-              <h2 className="text-2xl font-bold my-12">
-                Skills
-              </h2>
+              <h2 className="text-2xl font-bold my-12">My stack</h2>
             </div>
-            <div className="flex flex-col gap-4 overflow-hidden scroll-m-5 relative" id="about-info">
-              {<AnimatedRow elements={firstRowSkills} renderElement={(skill) => <SkillCard skill={skill} />} />}
-              {<AnimatedRow elements={secondRowSkills} animationDirection="left" renderElement={(skill) => <SkillCard skill={skill} />} />}
-
+            <div
+              className="flex flex-col gap-4 overflow-hidden scroll-m-5 relative"
+              id="about-info"
+            >
+              {
+                <AnimatedRow
+                  elements={firstRowSkills}
+                  renderElement={(skill) => <SkillCard skill={skill} />}
+                />
+              }
+              {
+                <AnimatedRow
+                  elements={secondRowSkills}
+                  animationDirection="left"
+                  renderElement={(skill) => <SkillCard skill={skill} />}
+                />
+              }
             </div>
           </div>
         </div>
